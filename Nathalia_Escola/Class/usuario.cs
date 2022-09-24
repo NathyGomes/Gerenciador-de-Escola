2be665d4d8987592e898404eaf0c55f800d4f09c
NathyGomez <nathaliagomes450@gmail.com>
2022-09-24 16:17:21 -0300

    Adicionar arquivos de projeto.

---
+++ Nathalia_Escola/Class/usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nathalia_Escola.Class
+{
+    public class usuario
+    {
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string celular { get; set; }
+        public string genero { get; set; }
+
+    }
+}
---
+++ Nathalia_Escola/JavaScript/javascript.js
@@ -0,0 +1,13 @@
+﻿var password = document.getElementById("password")
+    , Confirmpassword = document.getElementById("Confirmpassword");
+
+function validatePassword() {
+    if (password.value != Confirmpassword.value) {
+        Confirmpassword.setCustomValidity("Senhas diferentes!");
+    } else {
+        Confirmpassword.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+Confirmpassword.onkeyup = validatePassword;
